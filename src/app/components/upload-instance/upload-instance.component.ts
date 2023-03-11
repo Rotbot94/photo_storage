b9149d1e8151edcad4fff3716ff9dff4c3f4f699
Ali Issa <ali@dandesign.dk>
2023-03-11 12:02:48 +0100

    upload-instace files

---
+++ src/app/components/upload-instance/upload-instance.component.ts
@@ -0,0 +1,51 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {
+  ref,
+  Storage,
+  uploadBytesResumable,
+  UploadTask,
+  UploadTaskSnapshot,
+} from '@angular/fire/storage';
+import {Auth} from "@angular/fire/auth";
+
+@Component({
+  selector: 'app-upload-instance',
+  templateUrl: './upload-instance.component.html',
+  styleUrls: ['./upload-instance.component.scss'],
+})
+export class UploadInstanceComponent implements OnInit {
+
+  @Input() image: any;
+
+  percentage: any = 0;
+  snapshot$: UploadTaskSnapshot;
+
+  task$: UploadTask;
+
+  constructor(
+    private storage: Storage,
+    private auth: Auth,) {
+  }
+
+  ngOnInit() {
+    console.log(this.image);
+    this.startUpload().then(r => console.log('run'));
+  }
+
+  async startUpload() {
+    const user = this.auth.currentUser;
+    const path = `images/${user.uid}/${Date.now()}+1.webp`;
+    const storageRef = ref(this.storage, path);
+    const uploadTask = uploadBytesResumable(storageRef, this.image.blob);
+    this.task$ = uploadTask;
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        this.snapshot$ = snapshot;
+        this.percentage = ((snapshot.bytesTransferred / snapshot.totalBytes) * 100).toFixed(0);
+      });
+    this.task$.pause();
+  }
+}
+
+
+
